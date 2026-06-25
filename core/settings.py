3b1329f71b885cbf18332bbf3ec74b08a7b3df35
1olecram <marcelochave059@gmail.com>
2026-06-24 22:16:50 -0300

    fix: aceitar qualquer domínio

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-qh^b9)bc!z6x%h6$@+(i#$$(km$wm=ms0*3)23ywc+2q@1gl5-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
