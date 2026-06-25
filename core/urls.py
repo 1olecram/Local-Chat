6e0495b8cf6e46d6dd6d2413e642560b075b20f3
1olecram <marcelochave059@gmail.com>
2026-06-24 22:13:17 -0300

    fix: correcao das rotas

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
+from django.conf import settings
 
 from chat import views as chat_views
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/chat/')),
     path('admin/', admin.site.urls),
     path('api/', include('chat.urls')),
     path('login/', chat_views.LoginPageView, name='login-page'),
     path('chat/', chat_views.ChatPageView, name='chat-page'),
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico')),
 ]
